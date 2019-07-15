f9c6a208b3e42d6f1c49523a7bb17f91f7c2f762
NexayL <51516819+NexayL@users.noreply.github.com>
2019-07-15 20:58:00 +0200

    Update serverinfos.js

--- Commandes/serverinfos.js
+++ Commandes/serverinfos.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
-    .setDescription("Informations sur le serveur")
+    .setDescription("Informations sur Elitya")
     .setColor("#15f153")
     .setThumbnail(sicon)
     .addField("Nom du serveur", message.guild.name)
