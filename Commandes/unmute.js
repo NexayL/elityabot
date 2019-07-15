3876fff560f6477a86af1d11d7b484eccc3cdf7d
NexayL <51516819+NexayL@users.noreply.github.com>
2019-07-15 19:59:39 +0200

    Update unmute.js

--- Commandes/unmute.js
+++ Commandes/unmute.js
@@ -1,40 +1,23 @@
-const Discord = require('discord.js');
-const ms = require('ms');
+exports.run = (bot, message, args) => {
+    const discord = require("discord.js")
 
-module.exports.run = (client, message, args) => {
-          if (message.member.hasPermission("MANAGE_MESSAGES")) {
+    let User = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!User) return message.reply("**:x:** __**Erreur...**__**, Vous devez mentionner un utilisateur !**");
+    if(!User.roles.find(r => r.name == "MUET")) { return message.channel.send("**:x:** __**Erreur...**__**, ${member.user.username} N'est pas muet !**")
+    let muted = message.guild.roles.find(r => r.name === "MUET")
+    (User).removeRole(muted)
 
-            let unmuteRole = message.guild.roles.Find(`name`, "MUET");
-            let unmuteChannel = message.guild.channels.Find('name', "❌sanctions❌")
-            let unmuteReason = args.join(' ').clice(30)
-            let unmutedUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-            let member = message.mentions.members.first();
-            if (!unmutedUser) {
-                return message.channel.send("**:x:** __**Erreur...**__**, Vous devez mentionner un utilisateur !**")
-            }
-            let uuembed = new Discord.RichEmbed()
-            .setDescription("Unmute")
-            .setColor("#cd460b")
-            .addField("Utilisateur Unmute : ", `${unmutedUser}`)
-            .addField("Raison du unmute : ", unmuteReason)
-            .addField("Unmute par : ", `${message.author}`)
-            .setTimestamp();
-    
-            if (!member.roles.find("name", "MUET")) return message.channel.send(`**:x:** __**Erreur...**__**, ${member.user.username} N'est pas muet !**`).then(m => m.delete(20000));
-    
-            member.removeRole(unmuteRole.id);
-                
-            unmuteChannel.send(uuembed);
-            message.delete()
-        } else {
-            message.channel.send("**:x:** __**Erreur...**__**, Vous n'avez pas la permission !**")
-        }
+    let embed = new discord.RichEmbed()
+    .setDescription($(message.author.username)` Unmute ${User}`)
+    .setColor("#cd460b")
+    .addField("Utilisateur Unmute", User)
+    .addField("Unmute par", message.author.username)
+    .addField("Raison du unmute", reason)
+    let logchannel = message.guild.channel.find(c=> c.name === "❌sanctions❌")
 
-
-        if (!unmuteChannel) {
-            return message.channel.send("**:x:** __**Erreur...**__**, Salon de Sanctions introuvable !**")
-        };
-    };
-module.exports.help = {
-    name: 'unmute'
-};
+    logchannel.send(embed)
+    }
+}
+exports.help = {
+    name: "unmute"
+}
