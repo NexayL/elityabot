047b133158978bf2e3e54f8374009c26aeea86c8
NexayL <51516819+NexayL@users.noreply.github.com>
2019-07-15 20:57:42 +0200

    Update serverinfos.js

--- Commandes/serverinfos.js
+++ Commandes/serverinfos.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
-    .setDescription("Server Information")
+    .setDescription("Informations sur le serveur")
     .setColor("#15f153")
     .setThumbnail(sicon)
     .addField("Nom du serveur", message.guild.name)
