54fe9d80cafba62d389c1f55cb5aa2a5f1af22e0
NexayL <51516819+NexayL@users.noreply.github.com>
2019-07-15 20:23:10 +0200

    Update unmute.js

--- Commandes/unmute.js
+++ Commandes/unmute.js
@@ -3,21 +3,24 @@ exports.run = (bot, message, args) => {
 
     let User = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!User) return message.reply("**:x:** __**Erreur...**__**, Vous devez mentionner un utilisateur !**");
-    if(!User.roles.find(r => r.name == "MUET")) { return message.channel.send("**:x:** __**Erreur...**__**, ${member.user.username} N'est pas muet !**")
-    let muted = message.guild.roles.find(r => r.name === "MUET")
-    (User).removeRole(muted)
+    let reason = args[1];
+    console.log("After met")
+    let muted = message.guild.roles.find(`name`, "MUET");
+    if(!User.roles.has(muted.id)) return message.reply("**:x:** __**Erreur...**__**, ${member.user.username} N'est pas muet !**");
+    await(User.removeRole(muted.id));
 
     let embed = new discord.RichEmbed()
-    .setDescription($(message.author.username)` Unmute ${User}`)
+    .setDescription($(message.author.username)`Unmute ${User}`)
     .setColor("#cd460b")
-    .addField("Utilisateur Unmute", User)
+    .addField("Joueur unmute", User)
     .addField("Unmute par", message.author.username)
-    .addField("Raison du unmute", reason)
+    .addField("Raison", reason)
     let logchannel = message.guild.channel.find(c=> c.name === "❌sanctions❌")
 
     logchannel.send(embed)
+    console.log("after embed")
     }
-}
+
 exports.help = {
     name: "unmute"
 }
