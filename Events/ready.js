418d6006dea0dd1588247fd2053a3c4048b2f6e6
Nexay <51516819+NexayL@users.noreply.github.com>
2020-05-06 02:47:24 +0200

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,7 +1,7 @@
-module.exports = (client) => {
-    client.user.setPresence({
-        game: {
-            name: "?help For help"
-        }
-    });
-};
+//module.exports = (client) => {
+//    client.user.setPresence({
+//        game: {
+//            name: "?help For help"
+//        }
+//    });
+//};
